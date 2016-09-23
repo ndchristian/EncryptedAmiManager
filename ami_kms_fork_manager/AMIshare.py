96b8daeedfa6b949c99990a0cc3fe5f07a6b639d
Nicholas Christian <nd.christian@gmail.com>
2016-09-23 17:15:05 -0400

    Organize code and add JSON file output
    Clean up code and add JSON code output so data can easily be used
    better.

--- ami_kms_fork_manager/AMIshare.py
+++ ami_kms_fork_manager/AMIshare.py
@@ -10,6 +10,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+# -*- coding: utf-8 -*-
 
 from __future__ import print_function
 
@@ -22,32 +23,15 @@ import botocore
 # User must set path to the config file.
 CONFIG_PATH = 'config.json'
 
+MAIN_EC2_CLI = boto3.client('ec2')
+MAIN_STS_CLI = boto3.client('sts')
+MAIN_DYNA_CLI = boto3.client('dynamodb')
+MAIN_S3_CLI = boto3.client('s3')
 
-def share_ami():
-    """Adds permission for each account to be able to use the AMI."""
-
-    print("Sharing AMI...")
-    try:
-        main_ec2_cli.modify_image_attribute(
-            DryRun=True,
-            ImageId=ami_id,
-            OperationType='add',
-            UserIds=account_ids,
-            LaunchPermission={'Add': [dict(('UserId', account_number) for account_number in account_ids)]})
-    except Exception as e:
-        if e.response['Error']['Code'] == 'DryRunOperation':
-            main_ec2_cli.modify_image_attribute(
-                ImageId=ami_id,
-                OperationType='add',
-                UserIds=account_ids,
-                LaunchPermission={'Add': [dict(('UserId', account_number) for account_number in account_ids)]})
-        else:
-            raise e
-
-    print("Successfully shared the AMI.")
+REGION = boto3.session.Session().region_name
 
 
-def json_data():
+def config():
     """Grabs all data from the config file"""
     with open(CONFIG_PATH, 'r') as j:
         read_data = json.loads(j.read())
@@ -55,20 +39,42 @@ def json_data():
     return read_data
 
 
+def share_ami():
+    """Adds permission for each account to be able to use the AMI."""
+
+    print("Sharing AMI...")
+    try:
+        MAIN_EC2_CLI.modify_image_attribute(
+            DryRun=True,
+            ImageId=ami_id,
+            OperationType='add',
+            UserIds=account_ids,
+            LaunchPermission={'Add': [dict(('UserId', account_number) for account_number in account_ids)]})
+    except Exception as DryRunErr:
+        if DryRunErr.response['Error']['Code'] == 'DryRunOperation':
+            MAIN_EC2_CLI.modify_image_attribute(
+                ImageId=ami_id,
+                OperationType='add',
+                UserIds=account_ids,
+                LaunchPermission={'Add': [dict(('UserId', account_number) for account_number in account_ids)]})
+        else:
+            raise e
+
+
 def revoke_ami_access():
     """Revokes access to the specified AMI."""
 
     print("Revoking access to AMI...")
     try:
-        main_ec2_cli.modify_image_attribute(
+        MAIN_EC2_CLI.modify_image_attribute(
             DryRun=True,
             ImageId=ami_id,
             OperationType='remove',
             UserIds=account_ids,
             LaunchPermission={'Remove': [dict(('UserId', account_number) for account_number in account_ids)]})
-    except Exception as e:
-        if e.response['Error']['Code'] == 'DryRunOperation':
-            main_ec2_cli.modify_image_attribute(
+    except Exception as DryRunErr:
+        if DryRunErr.response['Error']['Code'] == 'DryRunOperation':
+            MAIN_EC2_CLI.modify_image_attribute(
                 ImageId=ami_id,
                 OperationType='remove',
                 UserIds=account_ids,
@@ -77,13 +83,26 @@ def revoke_ami_access():
             raise e
 
 
+def json_data_upload(json_data):
+    """Creates JSON file for computer reading."""
+    bucket_key = "%s/%s.html" % (config_data['General'][0]['JSON_S3keyLocation'], int(time.time()))
+
+    MAIN_S3_CLI.put_object(Bucket=config_data['General'][0]['JSON_S3bucket'],
+                           Key=bucket_key,
+                           Body=json.dumps(json_data,
+                                           sort_keys=True,
+                                           indent=4,
+                                           separators=(',', ': ')))
+    return bucket_key
+
+
 def create_html_doc(ami_details_list):
-    rollback_key_list = []
+    """Creates HTML document for human reading."""
 
     try:
-        image_description = image_details['Description']
+        image_desc = image_details['Description']
     except KeyError:
-        image_description = 'None'
+        image_desc = 'None'
 
     s3_input = """
     <!DOCTYPE html>
@@ -103,15 +122,17 @@ def create_html_doc(ami_details_list):
     """ % (image_details['ImageId'],
            image_details['Name'],
            config_data['General'][0]['OS'],
-           image_description,
+           image_desc,
            config_data['General'][0]['ReleaseDate'],
-           'arn:aws:ec2:%s::image/%s' % (region, image_details['ImageId']))
+           'arn:aws:ec2:%s::image/%s' % (REGION, image_details['ImageId']))
 
     for ami_details in ami_details_list:
-        s3_input += "<p>Company_account_Number: %s | AMI: %s | ARN:arn:aws:ec2:%s::image/%s</p>\n\n " % (
-            ami_details['AccountNumber'],
-            ami_details['AMI_ID'],
-            ami_details['Region'], ami_details['AMI_ID'])
+        s3_input += "<p>Company_account_Number: %s | AWS_Account_Number: %s | AMI: %s" \
+                    " | ARN:arn:aws:ec2:%s::image/%s</p>\n\n " % (
+                        config_data['General'][0]['CompanyAccountNumber'],
+                        ami_details['AccountNumber'],
+                        ami_details['AMI_ID'],
+                        ami_details['Region'], ami_details['AMI_ID'])
 
     s3_input += """
     </body>
@@ -119,60 +140,68 @@ def create_html_doc(ami_details_list):
 
     bucket_key = "%s/%s.html" % (config_data['General'][0]['HTML_S3keyLocation'], int(time.time()))
 
-    main_s3_cli.put_object(Bucket=config_data['General'][0]['HTML_S3bucket'],
+    MAIN_S3_CLI.put_object(Bucket=config_data['General'][0]['HTML_S3bucket'],
                            Key=bucket_key,
                            Body=s3_input)
 
-    rollback_key_list.append(bucket_key)
-
-    return rollback_key_list
+    return bucket_key
 
 
-def rollback(amis, put_items, keys):
+def rollback(amis, put_items, html_keys, json_keys):
     """Rollbacks all AWS actions done in case something goes wrong."""
     print("Rolling back...")
     revoke_ami_access()
 
     try:
-        table = main_dyna_cli.Table(config_data['General'][0]['DynamoDBTable'])
-        for put_item in put_items:
-            table.delete_item(Key=put_item)
-    except botocore.exceptions.ClientError as e:
-        print(e)
+        rollback_table = MAIN_DYNA_CLI.Table(config_data['General'][0]['DynamoDBTable'])
+        for rollback_item in put_items:
+            rollback_table.delete_item(Key=rollback_item)
+    except botocore.exceptions.ClientError as BotoError:
+        print(BotoError)
         pass
 
-    for key in keys:
-        main_s3_cli.delete_object(Bucket=config_data['General'][0]['HTML_S3bucket'],
-                                  Key=key)
+    for html_key in html_keys:
+        MAIN_S3_CLI.delete_object(Bucket=config_data['General'][0]['HTML_S3bucket'],
+                                  Key=html_key)
+    for json_key in json_keys:
+        MAIN_S3_CLI.delete_object(Bucket=config_data['General'][0]['JSON_S3bucket'],
+                                  Key=json_key)
 
-    for account in amis:
+    for rollback_account in amis:
         # STS allows you to connect to other accounts using assumed roles.
 
-        assumed_role = main_sts_cli.assume_rule(
-            RoleArn="arn:aws:iam::%s:role/%s" % (account['AccountNumber'], role_name),
+        rollback_assume_role = MAIN_STS_CLI.assume_rule(
+            RoleArn="arn:aws:iam::%s:role/%s" % (rollback_account['Accou'
+                                                                  'ntNumber'], role_name),
             RoleSessionName="AssumedRoleSession%s" % int(time.time()))
 
-        role_credentials = assumed_role['Credentials']
+        rollback_role_credentials = rollback_assume_role['Credentials']
 
-        session = boto3.Session(
-            aws_access_key_id=role_credentials['AccessKeyId'],
-            aws_secret_access_key=role_credentials['SecretAccessKey'],
-            aws_session_token=role_credentials['SessionToken'])
+        rollback_session = boto3.Session(
+            aws_access_key_id=rollback_role_credentials['AccessKeyId'],
+            aws_secret_access_key=rollback_role_credentials['SecretAccessKey'],
+            aws_session_token=rollback_role_credentials['SessionToken'])
 
-        ec2_cli = session.client('ec2', region_name=account['Region'])
+        rollback_ec2_cli = rollback_session.client('ec2', region_name=rollback_account['Region'])
 
         for image_to_delete in amis:
-            ec2_cli.deregister_image(ImageId=image_to_delete['AMD_ID'])
+            rollback_ec2_cli.deregister_image(ImageId=image_to_delete['AMD_ID'])
 
     print("Finished rolling back.")
 
 
-def main_share_amis():
-    """Main function that shares,copies, and encrypts the AMI in a new account. Also adds specified data into
-    DynamoDB."""
+if __name__ == '__main__':
+    config_data = config()
+
+    ami_id = config_data['General'][0]['AMI_ID']
+    role_name = config_data['General'][0]['RoleName']
+    account_ids = [account['AccountNumber'] for account in config_data['Account']]
+    image_details = MAIN_EC2_CLI.describe_images(ImageIds=[ami_id])['Images'][0]
 
     put_item_list = []
     ami_list = []
+    html_doc_list = []
+    json_doc_list = []
 
     try:
         share_ami()
@@ -184,7 +213,7 @@ def main_share_amis():
     for account_id in account_ids:
 
         # STS allows you to connect to other accounts using assumed roles.
-        assumed_role = main_sts_cli.assume_rule(
+        assumed_role = MAIN_STS_CLI.assume_rule(
             RoleArn="arn:aws:iam::%s:role/%s" % (account_id, role_name),
             RoleSessionName="AssumedRoleSession%s" % int(time.time()))
 
@@ -214,69 +243,60 @@ def main_share_amis():
                         for data in config_data['Accounts']:
                             if account_id == data['AccountNumber']:
                                 encrypted_ami = ec2_cli.copy_image(
-                                    SourceRegion=region,
+                                    SourceRegion=REGION,
                                     SourceImageId=ami_id,
                                     Name=image_details['Name'],
                                     Description=image_description,
                                     Encrypted=True,
-                                    KmsKeyId=config_data['RegionEncryptionKeys'][0][region])
+                                    KmsKeyId=config_data['RegionEncryptionKeys'][0][REGION])
 
                             ami_list.append({'AccountNumber': account_num,
-                                             'Region': region,
+                                             'Region': REGION,
                                              'AMI_ID': encrypted_ami['ImageId']})
                             print("Created encrypted AMI for %s." % data['AccountNumber'])
 
                     except botocore.exceptions.ClientError as e:
                         print(e)
-                        rollback(amis=ami_list, put_items=put_item_list, keys=None)
+                        rollback(amis=ami_list, put_items=put_item_list, html_keys=[], json_keys=[])
 
-                    # Adds specified data to DynamoDB. This will need to be changed for each person.
-                    put_items = {
-                        "sourceami": ami_id,
-                        "targetami": encrypted_ami['ImageId'],
-                        "targetregion": region_data,
-                        "targetawsaccountnum": account_num,
-                        "companyaccountnum": config_data['General'][0]['CompanyAccountNumber'],
-                        "releasedate": config_data['General'][0]['ReleaseDate'],
-                        "amiversionnum": config_data['General'][0]['AmiVersionNumber'],
-                        "stasisdate": config_data['General'][0]['StasisDate'],
-                        "os": config_data['General'][0]['OS'],
-                        "osver": config_data['General'][0]['OsVersion'],
-                        "comments:": config_data['General'][0]['Comments'],
-                        "jobnum": 'jobnum-%s' % int(time.time()),
-                        "logicaldelete": 0
+                    html_doc_list.append(create_html_doc(ami_details_list=ami_list))
+
+                    put_item_list.append({
+                        'sourceami': ami_id,
+                        'targetami': encrypted_ami['ImageId'],
+                        'targetregion': region_data,
+                        'targetawsaccountnum': account_num,
+                        'companyaccountnum': config_data['General'][0]['CompanyAccountNumber'],
+                        'releasedate': config_data['General'][0]['ReleaseDate'],
+                        'amiversionnum': config_data['General'][0]['AmiVersionNumber'],
+                        'stasisdate': config_data['General'][0]['StasisDate'],
+                        'os': config_data['General'][0]['OS'],
+                        'osver': config_data['General'][0]['OsVersion'],
+                        'comments:': config_data['General'][0]['Comments'],
+                        'jobnum': 'jobnum-%s' % int(time.time()),
+                        'epochtime': int(time.time()),
+                        'logicaldelete': 0
+                    })
+
+                    j_data = {
+                        'companyaccountnumber': config_data['General'][0]['CompanyAccountNumber'],
+                        'awsaccountnumber': account_num,
+                        'sourceami': ami_id,
+                        'targetami': encrypted_ami['ImageId'],
+                        'os': config_data['General'][0]['OS'],
+                        'osver': config_data['General'][0]['OsVersion'],
+
                     }
 
-                    key_data = create_html_doc(ami_details_list=ami_list)
+                    json_doc_list.append(json_data_upload(json_data=j_data))
 
-                    try:
-                        table = main_dyna_cli.Table(config_data['General'][0]['DynamoDBTable'])
-                        table.put_item(put_items)
-                        put_item_list.append(put_items)
-                    except Exception as e:  # General exception until a more specific, not even sure if it's needed
-                        print(e)
-                        rollback(amis=ami_list, put_items=put_item_list, keys=key_data)
-
-
-if __name__ == '__main__':
-
-    main_sts_cli = boto3.client('sts')
-    main_ec2_cli = boto3.client('ec2')
-    main_dyna_cli = boto3.client('dynamodb')
-    main_s3_cli = boto3.client('s3')
-
-    region = boto3.session.Session().region_name
-
-    config_data = json_data()
-
-    ami_id = config_data['General'][0]['AMI_ID']
-    role_name = config_data['General'][0]['RoleName']
-    account_ids = [account['AccountNumber'] for account in config_data['Account']]
-    image_details = main_ec2_cli.describe_images(ImageIds=[ami_id])['Images'][0]
-
-    try:
-        main_share_amis()
-    finally:
-        revoke_ami_access()
+    # Adds entries into a DyanomoDB database
+    for put_item in put_item_list:
+        try:
+            table = MAIN_DYNA_CLI.Table(config_data['General'][0]['DynamoDBTable'])
+            table.put_item(put_item)
+        except Exception as e:  # General exception until a more specific, not even sure if it's needed
+            print(e)
+            rollback(amis=ami_list, put_items=put_item_list, html_keys=html_doc_list, json_keys=json_doc_list)
 
     print("Done!")
