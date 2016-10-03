223be2629305e5d619aa4d004d81b4c9f9c32987
Nicholas Christian <nd.christian@gmail.com>
2016-10-03 11:21:12 -0400

    Get rid of vpc_exists waiter

--- ami_kms_fork_manager/AMIshare.py
+++ ami_kms_fork_manager/AMIshare.py
@@ -47,7 +47,6 @@ def create_vpc(function_ec2_cli):
         print("\tCreating temporary VPC...")
         temp_vpc = function_ec2_cli.create_vpc(CidrBlock='10.0.0.0/16')
         function_ec2_cli.get_waiter('vpc_available').wait(VpcIds=[temp_vpc['Vpc']['VpcId']])
-        function_ec2_cli.get_waiter('vpc_exists').wait(VpcIds=[temp_vpc['Vpc']['VpcId']])
         print("\tCreated VPC: %s" % temp_vpc['Vpc'][0]['VpcId'])
     except botocore.exceptions.ClientError as VpcError:
         function_ec2_cli.delete_vpc(VpcId = [temp_vpc['Vpc']['VpcId']])
