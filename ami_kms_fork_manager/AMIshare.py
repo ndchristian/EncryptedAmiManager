cd8e1370290b86a044f5d49c2f22596cdb869fce
Nicholas Christian <nd.christian@gmail.com>
2016-10-03 11:28:36 -0400

    No need to delete the VPC if it's never created

--- ami_kms_fork_manager/AMIshare.py
+++ ami_kms_fork_manager/AMIshare.py
@@ -50,9 +50,8 @@ def create_vpc(function_ec2_cli):
         print("\tCreated VPC: %s" % temp_vpc['Vpc']['VpcId'])
 
         return temp_vpc['Vpc']['VpcId']
-    
+
     except botocore.exceptions.ClientError as VpcError:
-        function_ec2_cli.delete_vpc(VpcId=[temp_vpc['Vpc']['VpcId']])
         raise VpcError
 
 
