1dc316bf4f03396f569768dcb87385e244c14fc9
Nicholas Christian <nd.christian@gmail.com>
2016-10-05 15:48:07 -0400

    Debugging print statement and switch to name

--- ami_kms_fork_manager/AMIshare.py
+++ ami_kms_fork_manager/AMIshare.py
@@ -150,6 +150,7 @@ def recreate_image(ami, function_ec2_cli, function_iam_cli, securitygroup_id, fu
 
     temp_sg_details = function_ec2_cli.describe_security_groups(GroupIds=[securitygroup_id])
     profile_details = function_iam_cli.get_role(RoleName=config_data['General'][0]['RoleName'])
+    print(profile_details)
 
     while True:
         try:
@@ -161,8 +162,8 @@ def recreate_image(ami, function_ec2_cli, function_iam_cli, securitygroup_id, fu
                                                            SubnetId=funct_subnet_id,
                                                            InstanceType='t2.micro',
                                                            IamInstanceProfile={
-                                                               'Name': profile_details['Role']['RoleName'],
-                                                               'Arn': profile_details['Role']['Arn']})
+                                                               'Name': profile_details['Role']['RoleName']
+                                                           })
 
             function_ec2_cli.get_waiter('instance_running').wait(
                 InstanceIds=[temp_instance['Instances'][0]['InstanceId']])
